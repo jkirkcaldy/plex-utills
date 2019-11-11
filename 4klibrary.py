ee9107dea027c34518ec577cc29d4691bbc2c4a2
jkirkcaldy <josh.kirkcaldy@gmail.com>
2019-11-11 18:19:06 +0000

    Create 4klibrary.py

---
+++ 4klibrary.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+from plexapi.server import PlexServer
+
+def to_tags(labels):
+    return [l.tag for l in labels]
+
+if __name__ == "__main__":
+    baseurl = 'YOUR_PLEX_SERVER_URL'
+    token = 'YOUR_PLEX_TOKEN'
+    plex = PlexServer(baseurl, token)
+    movies_section = plex.library.section('Movies')
+    movies = defaultdict(list)
+    added = movies_section.search(sort='addedAt:desc')
+
+    for movie in added:
+        resolutions = {m.videoResolution for m in movie.media}
+        if len(resolutions) > 1 and '4k' in resolutions:
+            movie.split()
+
+    added = movies_section.search(sort='addedAt:desc')
+    for movie in added:
+        movies[movie.title].append(movie)
+
+    for title, entries in movies.items():
+        # add Transcodable
+        for entry in entries:
+            if 'Transcodable' not in to_tags(entry.labels) and entry.media[0].videoResolution != '4k':
+                entry.addLabel('Transcodable')
+            if 'Transcodable' in to_tags(entry.labels) and entry.media[0].videoResolution == '4k':
+                entry.removeLabel('Transcodable')
+        # find the best
+        existing_bests = [e for e in entries if 'Best' in to_tags(e.labels)]
+        uhd_version = next((e for e in entries if e.media[0].videoResolution == '4k'), None)
+        # assume uhd version is the best if there is one, otherwise use bitrate
+        if uhd_version is not None:
+            best = uhd_version
+        else:
+            best = sorted(entries, key=lambda e: e.media[0].bitrate, reverse=True)[0]
+        if len(existing_bests) != 1 or existing_bests[0] != best:
+            for e in existing_bests:
+                e.removeLabel('Best')
+            best.addLabel('Best')
