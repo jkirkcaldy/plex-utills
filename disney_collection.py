e3f2302c125ba6e620eb90980d4f64458cf3fbba
jkirkcaldy <apps@themainframe.co.uk>
2020-02-28 13:12:50 +0000

    Create disney_collection.py

---
+++ disney_collection.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+from plexapi.server import PlexServer
+
+from configparser import ConfigParser
+
+#Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+server = config_object["PLEXSERVER"]
+
+baseurl = (server["PLEX_URL"])
+token = (server["TOKEN"])
+films = (server["FILMSLIBRARY"])
+plex = PlexServer(baseurl, token)
+movies_section = plex.library.section('films')
+movies = defaultdict(list)
+added = movies_section.search(sort='titleSort')
+
+
+for movie in added:
+    movies[movie.title].append(movie.studio)  
+    print('%s (%s)' % (movie.title, movie.studio)) 
+    if "Disney" in movie.studio:
+        movie.addCollection('Disney')
+    else:
+        pass    
