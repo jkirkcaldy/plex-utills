9a82589325190bdae717f6de773a9fce9987fa50
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-05-26 16:09:19 -0700

    Update 4k_hdr_poster.py
    Added function for 4K Dolby Vision posters

--- 4k_hdr_poster.py
+++ 4k_hdr_poster.py
@@ -24,6 +24,7 @@ films = plex.library.section(plexlibrary)
 banner_4k = Image.open("4K-Template.png")
 banner_hdr = Image.open("hdr-poster.png")
 banner_4k_hdr = Image.open("4k-hdr-poster.png")
+banner_4k_dv = Image.open("4k-dv-poster.png")
 size = (911,1367)
  
 
@@ -55,6 +56,38 @@ def poster_4k_hdr():
     background = Image.open('poster.png')
     background = background.resize(size,Image.ANTIALIAS)
     background.paste(banner_4k_hdr, (0, 0), banner_4k_hdr)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    os.remove('poster.png') 
+    
+def poster_4k_dv():
+    print(i.title + ' 4k DV')    
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')  
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
+            print('Creating a backup file')
+            dest = shutil.copyfile(filename, newdir+'poster_bak.png')
+            os.chown(newdir+'poster_bak.png', 99, 100)
+            os.chmod(newdir+'poster_bak.png', 0o0666)
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.ANTIALIAS)
+    background.paste(banner_4k_dv, (0, 0), banner_4k_dv)
     background.save('poster.png')
     i.uploadPoster(filepath="poster.png")
     os.remove('poster.png') 
@@ -130,6 +163,8 @@ def poster_hdr():
        
 for i in films.search(resolution="4k", hdr=True):
     poster_4k_hdr()
+#for i in films.search(resolution="4k", hdr=False):
+#    poster_4k_dv()
 #for i in films.search(resolution="4k", hdr=False):
 #    poster_4k()
 #for i in films.search(resolution="1080,720", hdr=True):
