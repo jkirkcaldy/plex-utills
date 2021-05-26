e89289795bfa7226415ea1a64556bc6afb4e7c51
= <apps@themainframe.co.uk>
2021-05-26 22:40:31 +0100

    added new tests and new way of getting posters
    Checks whether the script is running on windows.
    
    New way of getting the poster should mean the error with missing poster.png is resolved.
    
    re-organisation of he script.

--- 4k_hdr_poster.py
+++ 4k_hdr_poster.py
@@ -8,7 +8,7 @@ import os
 import re
 import stat
 from configparser import ConfigParser
-
+import platform
 
 config_object = ConfigParser()
 config_object.read("config.ini")
@@ -18,108 +18,119 @@ token = (server["TOKEN"])
 plexlibrary = (server["FILMSLIBRARY"])
 ppath = (server["PLEXPATH"])
 mpath = (server["MOUNTEDPATH"])
-BACKUP_POSTER = (server["BACKUP_POSTER"])
+pbak = (server["POSTER_BU"])
 plex = PlexServer(baseurl, token)
 films = plex.library.section(plexlibrary)
 banner_4k = Image.open("4K-Template.png")
 banner_hdr = Image.open("hdr-poster.png")
 banner_4k_hdr = Image.open("4k-hdr-poster.png")
 size = (911,1367)
-
-
+ 
 
 def poster_4k_hdr():
-    print(i.title + ' 4k HDR')
-    newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
-    backup = os.path.exists(newdir+'poster_bak.png') 
-    if backup == False:    
-        imgurl = baseurl + i.thumb + '.png'
-        img = requests.get(imgurl, stream=True)
-        filename = "poster.png"
-        if img.status_code == 200:
-            img.raw.decode_content = True
-            with open(filename, 'wb') as f:
-                shutil.copyfileobj(img.raw, f)
-        
-        if BACKUP_POSTER == True:
+    print(i.title + ' 4k HDR')    
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')  
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
             print('Creating a backup file')
             dest = shutil.copyfile(filename, newdir+'poster_bak.png')
             os.chown(newdir+'poster_bak.png', 99, 100)
             os.chmod(newdir+'poster_bak.png', 0o0666)
-        else: 
-            print('Backup set to False, moving on')    
-        
-        background = Image.open('poster.png')
-        background = background.resize(size,Image.ANTIALIAS)
-        background.paste(banner_4k_hdr, (0, 0), banner_4k_hdr)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
-        os.remove('poster.png') 
-    else:
-        print('Backup File Exists, Skipping...')    
 
-def poster_4k():
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.ANTIALIAS)
+    background.paste(banner_4k_hdr, (0, 0), banner_4k_hdr)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    os.remove('poster.png') 
+           
+
+def poster_4k():   
     print(i.title + " 4K Poster")
-    newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
-    backup = os.path.exists(newdir+'poster_bak.png') 
-    if backup == False:    
-        imgurl = baseurl + i.thumb + '.png'
-        img = requests.get(imgurl, stream=True)
-        filename = "poster.png"
-        if img.status_code == 200:
-            img.raw.decode_content = True
-            with open(filename, 'wb') as f:
-                shutil.copyfileobj(img.raw, f)
-        
-        if BACKUP_POSTER == True:
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')    
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
             print('Creating a backup file')
             dest = shutil.copyfile(filename, newdir+'poster_bak.png')
             os.chown(newdir+'poster_bak.png', 99, 100)
             os.chmod(newdir+'poster_bak.png', 0o0666)
-        else: 
-            print('Backup set to False, moving on')    
-        
-        background = Image.open('poster.png')
-        background = background.resize(size,Image.ANTIALIAS)
-        background.paste(banner_4k, (0, 0), banner_4k_hdr)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
-        os.remove('poster.png') 
-    else:
-        print('Backup File Exists, Skipping...')  
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.ANTIALIAS)
+    background.paste(banner_4k, (0, 0), banner_4k)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    os.remove('poster.png') 
+           
+
+           
 def poster_hdr():
     print(i.title + " HDR Poster")
-    newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
-    backup = os.path.exists(newdir+'poster_bak.png') 
-    if backup == False:    
-        imgurl = baseurl + i.thumb + '.png'
-        img = requests.get(imgurl, stream=True)
-        filename = "poster.png"
-        if img.status_code == 200:
-            img.raw.decode_content = True
-            with open(filename, 'wb') as f:
-                shutil.copyfileobj(img.raw, f)
-        
-        if BACKUP_POSTER == True:
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')    
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
             print('Creating a backup file')
             dest = shutil.copyfile(filename, newdir+'poster_bak.png')
             os.chown(newdir+'poster_bak.png', 99, 100)
             os.chmod(newdir+'poster_bak.png', 0o0666)
-        else: 
-            print('Backup set to False, moving on')    
-        
-        background = Image.open('poster.png')
-        background = background.resize(size,Image.ANTIALIAS)
-        background.paste(banner_hdr, (0, 0), banner_4k_hdr)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
-        os.remove('poster.png') 
-    else:
-        print('Backup File Exists, Skipping...')  
 
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.ANTIALIAS)
+    background.paste(banner_hdr, (0, 0), banner_hdr)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    os.remove('poster.png') 
+           
+
+       
 for i in films.search(resolution="4k", hdr=True):
     poster_4k_hdr()
-for i in films.search(resolution="4k", hdr=False):
-    poster_4k()
-for i in films.search(resolution="1080,720", hdr=True):
-    poster_hdr()
+#for i in films.search(resolution="4k", hdr=False):
+#    poster_4k()
+#for i in films.search(resolution="1080,720", hdr=True):
+#    poster_hdr()
--- setup_helper.py
+++ setup_helper.py
@@ -1,6 +1,13 @@
+from pathlib import Path
+from PIL import Image
 from plexapi.server import PlexServer
-from configparser import ConfigParser
+import numpy as np
+import requests
+import shutil
 import os
+import re
+import stat
+from configparser import ConfigParser
 
 config_object = ConfigParser()
 config_object.read("config.ini")
@@ -16,6 +23,17 @@ print('Running this will output a file location from your film library set in th
 print('It will only work properly if you have used the correct plex naming protocols.')
 print('Searching...')
 
-media_location = films.search()
-filepath = os.path.dirname(os.path.dirname(os.path.dirname(media_location[0].media[0].parts[0].file)))
+media_location = films.search(resolution='4k')
+filepath = os.path.dirname(os.path.dirname(media_location[0].media[0].parts[0].file))
+print('Plex mount path is:')
 print(filepath)
+
+print('checking poster download permissions...')
+i = films.search(resolution='4k')
+imgurl = i[0].posterUrl
+img = requests.get(imgurl, stream=True)
+if img.status_code == 200:
+    print("You're good to go")
+elif img.status_code == 401:
+    print("Unable to download the image")
+
