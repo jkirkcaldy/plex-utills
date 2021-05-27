1317aedf5ea8812d7285750d8447b21ebbc26d15
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-05-26 22:32:35 -0700

    Update 4k_hdr_poster.py

--- 4k_hdr_poster.py
+++ 4k_hdr_poster.py
@@ -20,6 +20,7 @@ plexdvlibrary = (server["DVLIBRARY"])
 plexde4klibrary = (server["DE4KLIBRARY"])
 plexdelibrary = (server["DELIBRARY"])
 plexdklibrary = (server["DKLIBRARY"])
+plexnllibrary = (server["NLLIBRARY"])
 ppath = (server["PLEXPATH"])
 mpath = (server["MOUNTEDPATH"])
 pbak = (server["POSTER_BU"])
@@ -28,6 +29,7 @@ films = plex.library.section(plexlibrary)
 dvfilms = plex.library.section(plexdvlibrary)
 defilms = plex.library.section(plexdelibrary)
 dkfilms = plex.library.section(plexdklibrary)
+nlfilms = plex.library.section(plexnllibrary)
 de4kfilms = plex.library.section(plexde4klibrary)
 banner_4k = Image.open("4K-Template.png")
 banner_hdr = Image.open("hdr-poster.png")
@@ -36,6 +38,7 @@ banner_4k_dv = Image.open("4k-dv-poster.png")
 banner_4k_de = Image.open("4k-de-poster.png")
 banner_de = Image.open("DE-template.png")
 banner_dk = Image.open("dk-poster.png")
+banner_nl = Image.open("nl-poster.png")
 size = (911,1367)
  
 
@@ -264,7 +267,38 @@ def poster_dk():
     background.save('poster.png')
     i.uploadPoster(filepath="poster.png")
     os.remove('poster.png') 
-           
+     
+def poster_nl():   
+    print(i.title + " DE Poster")
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')    
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
+            print('Creating a backup file')
+            dest = shutil.copyfile(filename, newdir+'poster_bak.png')
+            os.chown(newdir+'poster_bak.png', 99, 100)
+            os.chmod(newdir+'poster_bak.png', 0o0666)
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.ANTIALIAS)
+    background.paste(banner_nl, (0, 0), banner_nl)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    os.remove('poster.png')
 
        
 for i in films.search(resolution="4k", hdr=True):
@@ -275,6 +309,8 @@ for i in defilms.search(resolution="1080,720,480", hdr=False):
     poster_de()
 for i in dkfilms.search(resolution="1080,720,480", hdr=False):
     poster_dk()
+for i in nlfilms.search(resolution="1080,720,480", hdr=False):
+    poster_nl()
 for i in dvfilms.search(resolution="4k", hdr=True):
     poster_4k_dv() 
 for i in de4kfilms.search(resolution="4k", hdr=True):
