e930c09b92ecd04b34426de816c79edabb5df3f0
jkirkcaldy <apps@themainframe.co.uk>
2021-09-29 16:13:12 +0100

    big update

--- Pixar_collection.py
+++ Pixar_collection.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/usr/bin/env python3
 from collections import defaultdict
 from plexapi.server import PlexServer
@@ -23,3 +24,33 @@ for movie in added:
     # Skip movie if there is no studio info
     except TypeError:
         continue
+=======
+#!/usr/local/bin/python
+from collections import defaultdict
+from plexapi.server import PlexServer
+from configparser import ConfigParser
+from datetime import datetime
+#Read config.ini file
+config_object = ConfigParser()
+config_object.read("/config/config.ini")
+server = config_object["PLEXSERVER"]
+
+baseurl = (server["PLEX_URL"])
+token = (server["TOKEN"])
+films = (server["FILMSLIBRARY"])
+plex = PlexServer(baseurl, token)
+movies_section = plex.library.section(films)
+added = movies_section.search(sort='titleSort')
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print(current_time, ": Pixar Collection script starting now")
+
+for movie in added:
+    try:
+        if "pixar" in movie.studio:
+            movie.addCollection('Pixar')
+            print('%s (%s)' % (movie.title, movie.studio))
+    # Skip movie if there is no studio info
+    except TypeError:
+        continue
+>>>>>>> Stashed changes
--- hide-4k.py
+++ hide-4k.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/usr/bin/env python3
 
 from collections import defaultdict
@@ -38,3 +39,43 @@ if __name__ == "__main__":
             print(movie)             
 
                     
+=======
+#!/usr/local/bin/python
+from collections import defaultdict
+from plexapi.server import PlexServer
+from datetime import datetime
+from configparser import ConfigParser
+import socket
+
+#Read config.ini file
+config_object = ConfigParser()
+config_object.read("/config/config.ini")
+server = config_object["PLEXSERVER"]
+options = config_object["OPTIONS"]
+optimise = str.lower((options["transcode"]))
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print(current_time, ": Hide 4k films script starting now")
+
+baseurl = (server["PLEX_URL"])
+token = (server["TOKEN"])
+films = (server["FILMSLIBRARY"])
+plex = PlexServer(baseurl, token)
+movies_section = plex.library.section(films)
+added = movies_section.search(resolution='4k', sort='addedAt')
+b = movies_section.search(label='untranscodable', sort='addedAt')
+
+for movie in added:
+    resolutions = {m.videoResolution for m in movie.media}
+    if len(resolutions) < 2 and '4k' in resolutions:
+        if optimise == 'false':
+            movie.addLabel('Untranscodable')   
+            print(movie.title+' has only 4k avaialble, setting untranscodable' )
+        elif optimise == 'true':
+            print('Sending', movie.title, 'to be transcoded')
+            movie.optimize(deviceProfile="Android", videoQuality=10)
+for movie in b:
+    resolutions = {m.videoResolution for m in movie.media}
+    if len(resolutions) > 1 and '4k' in resolutions:
+        movie.removeLabel('Untranscodable')
+>>>>>>> Stashed changes
